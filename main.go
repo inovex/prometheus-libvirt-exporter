83ff38f84d6ec36baeb5898bc8b270ccedc9bc72
Niklas Schwarz <schwarz.niklas@hotmail.de>
2025-01-22 13:48:57 +0100

    Update prometheus common library
    * Update prometheus common library
    * Replace deprecated promlog with promslog
    * Switch to standardlib log/slog instead of go-kit/log
    * Update dependencies

--- main.go
+++ main.go
@@ -7,12 +7,11 @@ import (
 
 	kingpin "github.com/alecthomas/kingpin/v2"
 	"github.com/digitalocean/go-libvirt"
-	"github.com/go-kit/log/level"
 	exporter "github.com/inovex/prometheus-libvirt-exporter/pkg/exporter"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/prometheus/common/promlog"
-	"github.com/prometheus/common/promlog/flag"
+	"github.com/prometheus/common/promslog"
+	"github.com/prometheus/common/promslog/flag"
 	"github.com/prometheus/common/version"
 	"github.com/prometheus/exporter-toolkit/web"
 	webflag "github.com/prometheus/exporter-toolkit/web/kingpinflag"
@@ -34,15 +33,15 @@ func main() {
 	).Default("/metrics").String()
 	toolkitFlags := webflag.AddFlags(kingpin.CommandLine, ":9177")
 
-	promlogConfig := &promlog.Config{}
+	promlogConfig := &promslog.Config{}
 	flag.AddFlags(kingpin.CommandLine, promlogConfig)
 	kingpin.Version(version.Print("libvirt_exporter"))
 	kingpin.HelpFlag.Short('h')
 	kingpin.Parse()
-	logger := promlog.New(promlogConfig)
+	logger := promslog.New(promlogConfig)
 
-	_ = level.Info(logger).Log("msg", "Starting libvirt_exporter", "version", version.Info())
-	_ = level.Info(logger).Log("msg", "Build context", "build_context", version.BuildContext())
+	logger.Info("Starting libvirt_exporter", "version", version.Info())
+	logger.Info("Build context", "build_context", version.BuildContext())
 
 	exporter, err := exporter.NewLibvirtExporter(*libvirtURI, libvirt.ConnectURI(*driver), logger)
 	if err != nil {
@@ -65,7 +64,7 @@ func main() {
 		}
 		landingPage, err := web.NewLandingPage(landingCnf)
 		if err != nil {
-			_ = level.Error(logger).Log("err", err)
+			logger.Error("Failed to generate landing page", "msg", err)
 			os.Exit(1)
 		}
 		http.Handle("/", landingPage)
@@ -73,7 +72,7 @@ func main() {
 
 	srv := &http.Server{}
 	if err = web.ListenAndServe(srv, toolkitFlags, logger); err != nil {
-		_ = level.Error(logger).Log("err", err)
+		logger.Error("Failed to start server", "msg", err)
 		os.Exit(1)
 	}
 }
